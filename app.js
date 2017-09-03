8662fc121b3b0872bfb08499eefd4bcaa30ff3cb
Jennifer Johnson <jenjeep7@hotmail.com>
2017-09-02 19:13:07 -0500

    circle object created

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var app = angular.module("challengeApp", [])
 
 
-.controller("challengeController", function($scope) {
-      this.title = "RAZR Code Challenge"
-   });
+    .controller("challengeController", function () {
+        this.title = "RAZR Code Challenge";
+
+       function circleObject(x,y,r) {
+        this.radius = r;
+        this.yCoord = y;
+        this.xCoord = x;
+        this.getArea = function() {
+            return Math.PI*this.radius*2;
+       }
+       this.circleInfo = "Circle: Radius = "+ this.radius.toString() + " Area= " + this.getArea().toString();
+    
+       
+        }
+        var redCircle = new circleObject(30,14,14);
+        console.log(redCircle);
+    });
+
