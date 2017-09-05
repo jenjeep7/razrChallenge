e68e0d6fb165601777dd951e14e53a3e44419f82
Jennifer Johnson <jenjeep7@hotmail.com>
2017-09-04 19:11:03 -0500

    kind of rotating the squares as desired

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 var app = angular.module("challengeApp", [])
 
 
-    .controller("challengeController", function ($interval) {
+    .controller("challengeController", function ($scope, $interval) {
         this.title = "RAZR Code Challenge";
         this.name = "Jennifer Johnson";
-        this.shapeArray = [];
+        $scope.shapeArray = [];
 
-        function circleObject(r) {
-            this.radius = r / 2;
-            this.height = r;
+        function circleObject(h) {
+            this.radius = h / 2;
+            this.height = h;
             this.style = {
                 "background-color": "blue",
                 "border-radius": "50px",
@@ -22,7 +22,7 @@ var app = angular.module("challengeApp", [])
             this.Info = "Circle: Radius = " + this.radius.toString() + " Area = " + this.getArea().toString();
         }
 
-        function squareObject(h) {
+        function squareObject(h, rotate) {
             this.height = h;
             // this.yCoord = y;
             this.color = "red";
@@ -34,41 +34,53 @@ var app = angular.module("challengeApp", [])
                 "background-color": "red",
                 "border-radius": "0",
                 "height": this.height + "px",
-                "width": this.height + "px"
+                "width": this.height + "px",
+                "-ms - transform": "rotate(" + rotate + "deg)", /* IE 9 */
+                "-webkit - transform": "rotate(" + rotate + "deg)", /* Chrome, Safari, Opera */
+                "transform": "rotate(" + rotate + "deg)"
             }
             this.Area = this.getArea();
             this.Info = "Square: Size = " + this.height.toString() + " Area = " + this.getArea().toString();
-            
-        }
-        
-        // creates 50 new circle objects
-        for (var i = 0; i < 50; i++) {
-            var rr = Math.floor((Math.random() * 100) + 1);
-            var object = new circleObject(rr);
-            this.shapeArray.push({
-                object
-            });
 
-        };
-
-        // creates 50 new square objects
-        for (var i = 0; i < 50; i++) {
-            var h = Math.floor((Math.random() * 100) + 1);
-            var object = new squareObject(h);
-            this.shapeArray.push({
-                object
-            })
         }
 
-        // this sorts the array of objects by area but doesn't display them in order.  orderBy needed
-        this.sortArray = function () {
-            this.shapeArray.sort(function (a, b) {
-                return a.object.Area - b.object.Area;
-            });
 
-        };
 
-        this.sortArray();
+        $interval(function () {
+            $scope.shapeArray = [];
+            // creates 50 new circle objects
+            for (var i = 0; i < 50; i++) {
+                var rr = Math.floor((Math.random() * 100) + 1);
+                var object = new circleObject(rr);
+                $scope.shapeArray.push({
+                    object
+                });
+
+            };
+            var rotate = 10;
+            // creates 50 new square objects
+            for (var i = 0; i < 50; i++) {
+                var rotate = rotate + 10;
+                var h = Math.floor((Math.random() * 100) + 1);
+                var object = new squareObject(h,rotate);
+                $scope.shapeArray.push({
+                    object
+                })
+            }
+
+            // this sorts the array of objects by area but doesn't display them in order.  orderBy needed
+            this.sortArray = function () {
+                $scope.shapeArray.sort(function (a, b) {
+                    return a.object.Area - b.object.Area;
+                });
+
+            };
+
+            this.sortArray();
+
+        }, 500);
+
+
         // decided not to use draw and going with ng-style for the repeat
         // for (var i = 0; i < this.shapeArray.length; i++) {
         //     this.shapeArray[i].object.draw();
